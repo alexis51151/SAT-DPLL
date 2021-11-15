54fa06912ae9cbec88713c88ad08837e25c2f73b
alexis51151 <alexisdu51151@gmail.com>
2021-11-14 23:49:57 -0600

    Formula Generator done and tested.

---
+++ src/main/FormulaGenerator/RandomGenerator.java
@@ -0,0 +1,71 @@
+package FormulaGenerator;
+
+import Solver.CNF;
+import Solver.Clause;
+import Solver.Literal;
+import Solver.Prop;
+
+import java.util.*;
+
+public class RandomGenerator {
+    int N;              // Nb of variables
+    int K = 3;          // Nb of distinct literals per clause
+    int L;              // Nb of clauses
+    List<Prop> props;    // Set of variables
+    int seed = 1;       // Seed for randomization
+    Random rand = new Random(seed);
+
+    public RandomGenerator(int n, int l) {
+        N = n;
+        L = l;
+        List<Prop> props = new ArrayList<>();
+        // Generation of props
+        for (int i = 0; i < n; i++) {
+            // Name of the vars: x_i
+            props.add(new Prop("x" + i));
+        }
+        this.props = props;
+        assert N == this.props.size();
+    }
+
+    public List<Prop> getProps() {
+        return props;
+    }
+
+    /**
+     * 3-SAT generator function
+     * @return a randomly-generated 3-SAT formula
+     */
+    public CNF generate3SAT() {
+        // Check if we have enough variables
+        assert N > 2;
+        // Clauses construction
+        List<Clause> clauses = new ArrayList<>();
+        for (int i = 0; i < L; i++) {
+            for (int j = 0; j < 3; j++) {
+                // Consider Prop as a circular list
+                int k = rand.nextInt(N-1);  // Random integer between 0 and N-1: index in props
+                Literal x, y, z;
+                if (k < N-2) {
+                    x = new Literal(props.get(k), rand.nextBoolean());
+                    y = new Literal(props.get(k+1), rand.nextBoolean());
+                    z = new Literal(props.get(k+2), rand.nextBoolean());
+                } else {
+                    if (k == N-2) {
+                        x = new Literal(props.get(k), rand.nextBoolean());
+                        y = new Literal(props.get(k+1), rand.nextBoolean());
+                        z = new Literal(props.get(0), rand.nextBoolean());
+                    } else  {
+                        x = new Literal(props.get(k), rand.nextBoolean());
+                        y = new Literal(props.get(0), rand.nextBoolean());
+                        z = new Literal(props.get(1), rand.nextBoolean());
+                    }
+                }
+                assert x != y && x != z && y != z;
+                clauses.add(new Clause(new ArrayList<>(Arrays.asList(x,y,z))));
+            }
+        }
+        return new CNF(clauses);
+    }
+
+}
---
+++ src/test/FormulaGenerator/RandomGeneratorTest.java
@@ -0,0 +1,32 @@
+package FormulaGenerator;
+
+import Solver.CNF;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomGeneratorTest {
+
+    @Test
+    void generate3SATTest1() {
+        RandomGenerator generator = new RandomGenerator(3,1);
+        CNF cnf = generator.generate3SAT();
+        System.out.println(cnf);
+    }
+
+    @Test
+    void generate3SATTest2() {
+        RandomGenerator generator = new RandomGenerator(50,5);
+        CNF cnf = generator.generate3SAT();
+        System.out.println(cnf);
+    }
+
+    @Test
+    void generate3SATTest3() {
+        RandomGenerator generator = new RandomGenerator(150,20);
+        CNF cnf = generator.generate3SAT();
+        System.out.println(cnf);
+    }
+
+
+}
